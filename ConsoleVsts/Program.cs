8f8333ef42f3115d3fba5f681622bf2f2711072c
Fabricio Catae <fcatae@microsoft.com>
2018-04-07 17:18:04 -0300

    add relationship

--- ConsoleVsts/Program.cs
+++ ConsoleVsts/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleVsts
 
             //vsts.GetItemAsync(1, args[0], args[1], args[2]).Wait();
 
-            vsts.GetDeepItemAsync(2, args[0], args[1], args[2]).Wait();
+            vsts.AddItemRelationAsync(1, args[0], args[1], args[2]).Wait();
 
             //vsts.DiscoverTypesAsync(args[0], args[1], args[2]).Wait();
 
--- ConsoleVsts/VisualStudioRepository.cs
+++ ConsoleVsts/VisualStudioRepository.cs
@@ -44,7 +44,7 @@ namespace ConsoleVsts
             var t = await client.CreateWorkItemAsync(patchDocument, project, workType);
         }
 
-        public async Task GetItemAsync(int id, string vstsAccount, string project, string personalAccessToken)
+        public async Task<WorkItem> GetItemAsync(int id, string vstsAccount, string project, string personalAccessToken)
         {
             var accountUri = new Uri(vstsAccount);
             var connection = new VssConnection(accountUri, new VssBasicCredential(string.Empty, personalAccessToken));
@@ -52,6 +52,8 @@ namespace ConsoleVsts
             var client = connection.GetClient<WorkItemTrackingHttpClient>();
 
             var t = await client.GetWorkItemAsync(id);
+
+            return t;
         }
 
         public async Task GetDeepItemAsync(int id, string vstsAccount, string project, string personalAccessToken)
@@ -93,6 +95,42 @@ namespace ConsoleVsts
             var client = connection.GetClient<WorkItemTrackingHttpClient>();
 
             var result = await client.QueryByWiqlAsync(wiql);
+        }
+        
+        public async Task AddItemRelationAsync(int parentId, string vstsAccount, string project, string personalAccessToken)
+        {
+            var parent = await GetItemAsync(parentId, vstsAccount, project, personalAccessToken);
+            string parentUrl = ((ReferenceLink)parent.Links.Links["self"]).Href;
+
+            var accountUri = new Uri(vstsAccount);
+            var connection = new VssConnection(accountUri, new VssBasicCredential(string.Empty, personalAccessToken));
+
+            var client = connection.GetClient<WorkItemTrackingHttpClient>();
+
+            var patchDocument = new JsonPatchDocument();
+
+            patchDocument.Add(
+                new JsonPatchOperation()
+                {
+                    Operation = Operation.Add,
+                    Path = "/fields/System.Title",
+                    Value = "Linked task"
+                }
+            );
+            patchDocument.Add(
+                new JsonPatchOperation()
+                {
+                    Operation = Operation.Add,
+                    Path = "/relations/-",
+                    Value = new {
+                        rel = "System.LinkTypes.Hierarchy-Reverse",
+                        url = parentUrl
+                    }
+                }
+            );
+
+            string workType = "Task";
+            var t = await client.CreateWorkItemAsync(patchDocument, project, workType);
         }
     }
 }
